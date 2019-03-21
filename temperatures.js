da375be52dbc9e31eb7d4a87acdedd50766944ed
Fred <frederic.lavrut@outlook.com>
2019-03-21 10:12:47 +0100

    Update temperatures.js

--- temperatures.js
+++ temperatures.js
@@ -3,4 +3,4 @@ const temps = readline().split(' '); // the n temperatures expressed as integers
 const positiveTemps = temps.filter((v) => v >= 0).sort((a, b) => a - b); // split and sort values to positive only
 const negativeTemps = temps.filter((v) => v < 0).sort((a, b) => b - a); // split and sort values to negative only
 const nearestFromZero = positiveTemps[0] <= negativeTemps[0] * -1 ? positiveTemps[0] : negativeTemps[0];
-print(nearestFromZero != undefined ? nearestFromZero : 0);
+print(nearestFromZero != undefined ? nearestFromZero : positiveTemps[0] || 0);
